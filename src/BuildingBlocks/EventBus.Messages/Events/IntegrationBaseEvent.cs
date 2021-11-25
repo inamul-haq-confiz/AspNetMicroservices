5a0cfd670ab736f8926f24ca17b4e06faf23aac7
inamul-haq-confiz <inamul.haq@confiz.com>
2021-11-25 22:03:54 +0500

    gateway and ordering microservices

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EventBus.Messages.Events
+{
+	public class IntegrationBaseEvent
+	{
+		public IntegrationBaseEvent()
+		{
+			EventId = Guid.NewGuid();
+			CreationDate = DateTime.UtcNow;
+		}
+
+		public IntegrationBaseEvent(Guid id, DateTime createDate)
+		{
+			EventId = id;
+			CreationDate = createDate;
+		}
+		[IgnoreDataMember]
+		public Guid EventId { get; private set; }
+
+		public DateTime CreationDate { get; private set; }
+	}
+}
